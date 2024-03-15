c55e06315cb7cd9272a183395e5f416ffcd3997e
MatiasPhoenix <matias.la19@gmail.com>
2024-03-15 16:43:38 +0100

    componenti-basi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { DashboardComponent } from './Componenti/dashboard/dashboard.component';
     AttivitaGiornaliereComponent,
     BarraInferioreComponent,
     InGameComponent,
-    DashboardComponent
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
